2b163a3e310a1c955a422003985d31acd772c5d9
Joseph Moon <moon@college.harvard.edu>
2013-07-30 18:31:58 -0400

    Sanity commit 2 july 30, 2013

--- src/distance.py
+++ src/distance.py
@@ -6,13 +6,20 @@ Abstract distance module providing different notions of distance
 import abc 
 from abc import ABCMeta
 import numpy 
+from numpy import array 
+import sklearn as sk 
+
+#mi-based distances from scikit-learn 
+from sklearn.metrics import mutual_info_score, normalized_mutual_info_score, adjusted_mutual_info_score 
 
 class CDistance:
 	''' 
 	abstract distance, handles numpy arrays (probably should support lists for compatibility issues)
 	'''
 	__metaclass__ = ABCMeta
-	
+		
+	c_hashInvertFunctions = {"logistic": lambda x:1.0/(1+numpy.exp(-1.0*x)), "flip": lambda x: -1.0*x, "1mflip": lambda x: 1-1.0*x }
+
 	class EDataType:
 		DISCRETE = 0 
 		CONTINUOUS = 1
@@ -22,15 +29,17 @@ class CDistance:
 	def __init__( self, c_array1, c_array2 ): 
 		self.m_data1 = c_array1
 		self.m_data2 = c_array2 
-
+ 
+	def get_inverted_distance( self, strFunc = None ):
+		pFunc = CDistance.c_hashInvertFunctions[strFunc or "flip"] 
+		return pFunc( self.get_distance() ) 
+						
 	@abc.abstractmethod
 	def get_distance( self ): pass 
 
 	@abc.abstractmethod 
 	def get_distance_type( self ): pass
-
-	def get_data_type( self ) : pass 
-		
+	
 	
 class CEuclideanDistance( CDistance ):
 
@@ -45,14 +54,53 @@ class CEuclideanDistance( CDistance ):
 		return numpy.linalg.norm( self.m_data2-self.m_data1 ) 
 
 	def get_distance_type( self ):
-		return self.c_distance_type 
+		return self.c_distance_type 	
 	
-	@abc.abstractmethod 
-	def get_data_type( self ): pass 
+class CMutualInformation( CDistance ):
+	
+	__metaclass__ = ABCMeta 
 
-a1, a2 = numpy.array( [1,2,3,4,5] ), numpy.array( [6,7,8,9,10] ) 
+	def __init__( self, c_array1, c_array2 ):
+		self.m_data1 = c_array1 
+		self.m_data2 = c_array2 
+		self.c_distance_type = CDistance.EMetricType.NONMETRIC 
+	
+	def get_distance( self ):
+		assert( numpy.shape(self.m_data1) == numpy.shape(self.m_data2) )
+		return mutual_info_score( self.m_data1, self.m_data2 )
+	
+	def get_distance_type( self ):
+		return self.c_distance_type 	
+	
+class CNormalizedMutualInformation( CDistance ):
+	
+	__metaclass__ = ABCMeta 
 
-CE = CEuclideanDistance( a1, a2 )
-print CE.get_distance()
-print CE.get_distance_type() 
-print CE.get_data_type()  
+	def __init__( self, c_array1, c_array2 ):
+		self.m_data1 = c_array1 
+		self.m_data2 = c_array2 
+		self.c_distance_type = CDistance.EMetricType.NONMETRIC 
+	
+	def get_distance( self ):
+		assert( numpy.shape(self.m_data1) == numpy.shape(self.m_data2) )
+		return normalized_mutual_info_score( self.m_data1, self.m_data2 )
+
+	def get_distance_type( self ):
+		return self.c_distance_type 	
+	
+class CAdjustedMutualInformation( CDistance ):
+	
+	__metaclass__ = ABCMeta 
+
+	def __init__( self, c_array1, c_array2 ):
+		self.m_data1 = c_array1 
+		self.m_data2 = c_array2 
+		self.c_distance_type = CDistance.EMetricType.NONMETRIC 
+	
+	def get_distance( self ):
+		assert( numpy.shape(self.m_data1) == numpy.shape(self.m_data2) )
+		return adjusted_mutual_info_score( self.m_data1, self.m_data2 )
+
+	def get_distance_type( self ):
+		return self.c_distance_type 	
+	
