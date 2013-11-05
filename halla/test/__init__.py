2738809edc94d31a62be7b53d950ba6e5f957af2
Joseph Moon <moon.yosup@gmail.com>
2013-11-04 20:14:27 -0500

    Made some changes to test generating script

--- halla/test/__init__.py
+++ halla/test/__init__.py
@@ -6,9 +6,18 @@ import numpy as np
 from numpy import array 
 import scipy as sp 
 from scipy.stats import percentileofscore
+from numpy.random import normal, multinomial 
 
-def rand( tShape = (10,10), pDist = np.random.normal ):
-	
+
+#================================================================================
+# Base cases 
+#================================================================================
+
+def rand( tShape = (10,10), pDist = normal ):
+	"""
+	Returns a tShape-dimensional matrix given by base distribution pDist 
+	Order: Row, Col 
+	"""	
 	H = pDist #base measure 
 	
 	iRow, iCol = tShape
@@ -16,3 +25,30 @@ def rand( tShape = (10,10), pDist = np.random.normal ):
 	assert( iRow != 0 and iCol !=0 ) 
 	
 	return array( [[H() for i in range(iRow)] for j in range(iCol)] )
+
+def randmix( N, pDist = normal, atParam, tPi ):
+	"""
+	Returns N copies drawn from a mixture distribution $H$ 
+	Input: N <- number of components
+		pDist <- pointer to base distribution H 
+		atParam <- length $k$ parameters to distribution pDist, $\theta$  
+		tPi <- length $k$ tuple (vector) to categorical rv Z_n 
+
+	Output: N copies from mixture distribution $\sum_{k=1}^{K} \pi_k H(.| \theta )$ 
+	""" 
+	
+	assert( len( atParam ) == len( tPi ) )
+	
+	aOut = [] 
+
+	K = len( atParam ) 
+	H = pDist 
+	for n in range(N):
+		Zn = multinomial( 1, tPi )
+		aOut.append( H( *atParam[Zn] ) )
+
+#================================================================================
+# Special cases 
+#================================================================================
+
+		
