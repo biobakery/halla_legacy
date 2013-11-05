79b71da09ceb84f71ef39d848cef031c074d10ae
Joseph Moon <moon.yosup@gmail.com>
2013-11-04 22:13:48 -0500

    uniformly_spaced_gaussian implemented.

--- halla/test/__init__.py
+++ halla/test/__init__.py
@@ -7,13 +7,13 @@ from numpy import array
 import scipy as sp 
 from scipy.stats import percentileofscore
 from numpy.random import normal, multinomial 
-
+from itertools import compress 
 
 #================================================================================
 # Base cases 
 #================================================================================
 
-def rand( tShape = (10,10), pDist = normal ):
+def rand( tShape = (10,10), pDist = np.random.normal ):
 	"""
 	Returns a tShape-dimensional matrix given by base distribution pDist 
 	Order: Row, Col 
@@ -24,9 +24,9 @@ def rand( tShape = (10,10), pDist = normal ):
 
 	assert( iRow != 0 and iCol !=0 ) 
 	
-	return array( [[H() for i in range(iRow)] for j in range(iCol)] )
+	return array( [[H() for i in range(iCol)] for j in range(iRow)] )
 
-def randmix( N, pDist = normal, atParam, tPi ):
+def randmix( N, pDist, atParam, tPi ):
 	"""
 	Returns N copies drawn from a mixture distribution $H$ 
 	Input: N <- number of components
@@ -44,11 +44,19 @@ def randmix( N, pDist = normal, atParam, tPi ):
 	K = len( atParam ) 
 	H = pDist 
 	for n in range(N):
-		Zn = multinomial( 1, tPi )
-		aOut.append( H( *atParam[Zn] ) )
+		# multinomial returns boolean vector 
+		aParam = [x for x in compress( atParam, multinomial( 1, tPi ) )][0]
+		aOut.append( H( *aParam ) )
+	return aOut
 
 #================================================================================
 # Special cases 
 #================================================================================
 
-		
+def uniformly_spaced_gaussian( N, K = 4, fD = 2.0, tPi = (0.25,0.25,0.25,0.25) ):
+	"""
+	Generate uniformly spaced Gaussian, with spacing fD in the mean.
+	Constant 1.0 variance 
+
+	"""
+	return randmix( N, pDist = normal, atParam = [(m,1.0) for m in [fD*i for i in range(K)]], tPi = tPi )
