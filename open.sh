7329f64f6d2b3d82e0c96e0dbd5117c54da53be0
Joseph Moon <moon.yosup@gmail.com>
2014-02-26 13:26:10 -0500

    user: Joseph Moon <moon.yosup@gmail.com> branch 'default' removed doc/plan/01-22-14.rst removed doc/plan/01-27-14.rst removed doc/plan/01-30-14.rst removed doc/plan/07-12-13.rst removed doc/plan/09-19-13.rst removed doc/plan/11-6-13.rst removed doc/plan/12-18-13.rst removed doc/plan/organization.rst removed java/MINE.jar removed open.sh removed tmp/analyze.py removed tmp/cakecut.py removed tmp/clustering.py removed tmp/getMCA.R removed tmp/make_uncorrelated.py removed tmp/parse_discretize.py removed tmp/parse_discretize2.py

--- open.sh
+++
@@ -1,6 +0,0 @@
-## Convenient script to open all the source files I use regularly.
-
-## open ipython notebook 
-ipython notebook --pylab=inline & 
-
-subl halla/__init__.py halla/hierarchy/__init__.py halla/stats/__init__.py ../strudel/strudel.py 
