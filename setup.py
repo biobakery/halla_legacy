81512a826e7426a18594970b1ae50bf9e6733751
gholamali_rahnavard <gholamali_rahnavard@localhost>
2016-06-09 19:26:04 -0400

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ try:
     from setuptools import setup, find_packages
 except ImportError:
     sys.exit("Please install setuptools.")
-VERSION = "0.6.3"
+VERSION = "0.6.4"
 AUTHOR = "Gholamali Rahnavard, Curtis Huttenhower, Huttenhower Lab"
 AUTHOR_EMAIL = "halla-users@googlegroups.com"
 MAINTAINER = "Gholamali Rahnavard"
