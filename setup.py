24e08bec81adaa22842f56e3642d205460487c14
Lauren McIver <Lauren McIver@localhost>
2016-08-03 12:49:57 -0600

    Add counter since global pypi stats are currently turned off, this will be removed when pypi warehouse is production as it will have stats

--- setup.py
+++ setup.py
@@ -3,11 +3,30 @@ try:
     from setuptools import setup, find_packages
 except ImportError:
     sys.exit("Please install setuptools.")
+
+import os
+import urllib
+
 VERSION = "0.6.9"
 AUTHOR = "Gholamali Rahnavard, Curtis Huttenhower, Huttenhower Lab"
 AUTHOR_EMAIL = "halla-users@googlegroups.com"
 MAINTAINER = "Gholamali Rahnavard"
 MAINTAINER_EMAIL = "gholamali.rahnavard@gmail.com"
+
+# try to download the bitbucket counter file to count downloads
+# this has been added since PyPI has turned off the download stats
+# this will be removed when PyPI Warehouse is production as it
+# will have download stats
+COUNTER_URL="http://bitbucket.org/biobakery/halla/downloads/counter.txt"
+counter_file="counter.txt"
+if not os.path.isfile(counter_file):
+    print("Downloading counter file to track halla downloads"+
+        " since the global PyPI download stats are currently turned off.")
+    try:
+        file, headers = urllib.urlretrieve(COUNTER_URL,counter_file)
+    except EnvironmentError:
+        print("Unable to download counter")
+
 setup(
     name="halla",
     author=AUTHOR,
